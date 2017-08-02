df81fc4f18d3384eb7ee98f5d7281fbc527bb8e3
naitRAM <rmansieh@gmail.com>
2017-08-02 16:01:10 -0400

    working on majid-rahmat's shopping list

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
+$(document).ready(function(){
 $("button").click(function(e){
     e.preventDefault();
-    console.log("hello");//ajax code here
-    var name = $(".forms").val();
-    console.log(name);
-    var toAdd = $('.forms').val();
-    $(".added-items").append('<div class="item-group">' + '<input class="abs" type="checkbox">' + '<p class="inb">'+toAdd+ '</p>'+ '<i class="fa fa-trash-o fa-2x" aria-hidden="true">' +'</i>'+'</div>');
+    var todo = $(".forms").val();
+    $(".added-items").append('<div class="item-group">' + '<input class="abs" type="checkbox">' + '&nbsp;' + '&nbsp;' + '<p class="inb">' + todo + '</p>' + '&nbsp;' + '&nbsp;' + '<i class="fa fa-trash-o fa-2x" aria-hidden="true">' +'</i>'+'</div>');
+	});
+	$(document).on("click", "input[type='checkbox']", function() {
+	$(this).next().toggleClass("line-through");
+	});
+	$(document).on("click", ".fa-trash-o", function() {
+	$(this).parent().remove();
+	});
 });
-
 
